7db00886a99ea0d596c7a6f0e8aad61a732684e1
Your Name <dixitayush550@gmail.com>
2024-04-13 22:41:54 +0530

    adding continue with google button

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -14,6 +14,9 @@ export default function SignUp() {
       className='border p-3 rounded-lg' id='password' />
       <button className='bg-slate-700 text-white p-3 rounded-lg
       uppercase hover:opacity-95 disabled:opacity-80 '>Sign up</button>
+      <button className='bg-red-800 text-white p-3 rounded-lg
+      uppercase hover:opacity-95 disabled:opacity-80 '>Continue with Google</button>
+    
     </form>
     <div className='flex gap-2 mt-5'>
       <p>Have an account?</p>
