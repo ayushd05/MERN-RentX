02c6d6133c88b0a38fb85cf15a2e7bd853ba601b
Your Name <dixitayush550@gmail.com>
2024-04-15 23:57:02 +0530

    Complete Sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 
 export const signup = async (req, res, next) =>  {
@@ -14,3 +15,22 @@ export const signup = async (req, res, next) =>  {
         next(error);
     }
 };
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await  User.findOne({email});
+        if(!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(404, 'Wrong Credentials'));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
+        res
+        .cookie('access_token', token, { httpOnly : true })
+        .status(200)
+        .json(rest);
+    } catch (error) {
+        next(error);  
+    }
+
+}
