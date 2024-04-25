938d5149acee0eb6b0e91bd58990a252f31ea2e3
Your Name <dixitayush550@gmail.com>
2024-04-25 23:51:58 +0530

    Profile Page part 1

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,53 @@
-import { current } from '@reduxjs/toolkit'
-import { updateCurrentUser } from 'firebase/auth'
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useRef, useState, useEffect } from 'react'
+import {getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+import { app } from '../firebase'
+
 
 export default function Profile() {
+  const fileRef = useRef(null)
   const {currentUser}= useSelector((state)=> state.user)
+  const [file, setFile] = useState(undefined)
+  console.log(file)
+ 
+  //  firebase storage
+  //  allow read;
+  //  allow write: if
+  //  request.resource.size < 2 * 1024 * 1024 &&
+  //  request.resource.contentType.matches('image/ .')
+  useEffect(() => {
+    if(file){
+      handelFileUpload(file);
+    }
+  },[file]);
+
+  const handelFileUpload = (file) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred /
+      snapshot.totalBytes)* 100;
+      console.log('Upload is' + progress + '%done');
+      })
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center
       my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <img className="rounded-full h-25 w-25
+        <input 
+        onChange={(e)=> setFile(e.target.files[0])} 
+        type='file' 
+        ref={fileRef} 
+        hidden 
+        accept='image/*' />
+        <img onClick={()=> fileRef.current.click()}
+              className="rounded-full h-25 w-25
               object-cover cursor-pointer self-center mt-2" 
               src={currentUser.avatar} alt="profile"/>
               <input type="text" placeholder='username'
