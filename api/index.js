02f0f6666c3d75b59c417a566323f6f0adf7e148
Your Name <dixitayush550@gmail.com>
2024-04-27 07:03:25 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import Mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 Mongoose
@@ -17,6 +18,8 @@ Mongoose
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () =>{
     console.log("Server is on port 3000!!!!");
